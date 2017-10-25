737cc0041b690168f50c1a2e447d2c02cc29d395
Ricardo Valencia Quintero <ricardovalquin@gmail.com>
2017-10-24 21:40:50 -0500

    Created the video service and the getVideosByCategory method tests

---
+++ src/app/services/video.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { vimeoAPIConfig } from '../constants/environment-constants';
+
+@Injectable()
+export class VideoService {
+
+  private apiConfig = vimeoAPIConfig;
+
+  constructor(private http: Http) { }
+
+  getVideosByCategory(category: string, page: number): Promise<any[]> {
+    return this.http.get(`${this.apiConfig['apiBaseUrl']}categories/${category}/videos?page=${page}&per_page=
+    ${this.apiConfig['perPage']}&access_token=${this.apiConfig['accessToken']}`)
+      .toPromise()
+      .then(response => {
+        console.log(response);
+        return response.json().data;
+      });
+  }
+
+}
