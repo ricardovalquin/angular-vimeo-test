3cd9d03574514ccf72c6fdac24e48e5547e137a0
Ricardo Valencia Quintero <ricardovalquin@gmail.com>
2017-10-25 08:05:48 -0500

    Category service and the category model

---
+++ src/app/services/category/category.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { vimeoAPIConfig } from '../../constants/environment-constants';
+import {Category} from '../../models/category';
+
+
+@Injectable()
+export class CategoryService {
+
+  private apiConfig = vimeoAPIConfig;
+
+  constructor(private http: Http) { }
+
+  getAllCategories(): Promise<Category[]> {
+    return this.http.get(`${this.apiConfig['apiBaseUrl']}categories`)
+      .toPromise()
+      .then(response => response.json().data);
+  }
+
+}
